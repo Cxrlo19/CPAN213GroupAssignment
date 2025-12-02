4c354eb3af1ec6203cc26c70d0e9c0f563fbdc19
Carlo-N016 <129530980+Cxrlo19@users.noreply.github.com>
2025-12-02 15:35:35 -0500

    fixed the api issue

--- QuoteGenerator/utils/api.js
+++ QuoteGenerator/utils/api.js
@@ -1,18 +1,17 @@
 //Api fetch function to get the random quote
 export const getRandomQuote = async () => {
-    try{
-        const response = await fetch('https://api.quotable.io/random');
-        if(!response.ok){
+    try {
+        const response = await fetch('https://dummyjson.com/quotes/random');
+        if (!response.ok) {
             throw new Error('Could not fetch quote please try again later');
         }
         const data = await response.json();
         return {
-            content: data.content,
-            author : data.author,
+            content: data.quote,
+            author: data.author,
         };
-    }catch(error){
+    } catch (error) {
         console.error('Error fetching the quote:', error)
         throw error;
-        return null;
     }
 }
